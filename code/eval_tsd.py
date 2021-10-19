0923a2f5a40ecc8588fe3a21ea480c84ab0974d0
Jiang Yuxin 姜宇心 <70904956+YJiangcm@users.noreply.github.com>
2021-10-19 22:45:59 +0800

    Add files via upload

---
+++ code/eval_tsd.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import numpy as np 
+import sys
+import argparse
+from DataProcess import flair_pred2sample_pred
+
+def f1(predictions, gold):
+    """
+    F1 (a.k.a. DICE) operating on two lists of offsets (e.g., character).
+    >>> assert f1([0, 1, 4, 5], [0, 1, 6]) == 0.5714285714285714
+    :param predictions: a list of predicted offsets
+    :param gold: a list of offsets serving as the ground truth
+    :return: a score between 0 and 1
+    """
+    if len(gold) == 0:
+        return 1. if len(predictions) == 0 else 0.
+    if len(predictions) == 0:
+        return 0.
+    predictions_set = set(predictions)
+    gold_set = set(gold)
+    nom = 2 * len(predictions_set.intersection(gold_set))
+    denom = len(predictions_set) + len(gold_set)
+    return float(nom)/float(denom)
+
+
+
+def evaluate(pred_file, test_file):
+    '''
+    pred_file: path to the prediction file
+    test_file: path to the test csv file
+    '''
+    test_df = pd.read_csv(test_file)
+
+    gold_spans = test_df.spans.apply(eval).to_list()
+
+    pred_spans = [eval(line.strip()) for line in open(pred_file).readlines()]
+
+    if(len(gold_spans) != len(pred_spans)):
+        print('Error: the number of predictions does not match the number of test examples!')
+        sys.exit(1)
+
+
+    scores = []
+
+    for pred, gold in zip(pred_spans, gold_spans):
+        scores.append(f1(pred, gold))
+
+    print('F1 score: ',  np.mean(scores))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--flair_pred_file",
+                        required=True,
+                        help="path to the txt file that flair generates.")
+    
+    parser.add_argument("--sample_pred_file",
+                        required=True,
+                        help="save path of the txt file that is standard prediction.")
+    
+    parser.add_argument("--test_file",
+                        required=True,
+                        help="path to the csv file with gold spans.")
+    
+    args = parser.parse_args()
+    
+    flair_pred2sample_pred(args.flair_pred_file, args.sample_pred_file)
+    
+    evaluate(args.sample_pred_file, args.test_file)
+
+
+if __name__ == "__main__":
+    main()
